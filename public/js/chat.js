d047f4865e52c4d8e51500e0763c8186dccf99ef
jesus.acevedo <jesus.acevedo@globant.com>
2021-09-24 12:20:00 -0500

    initial-commit

---
+++ public/js/chat.js
@@ -0,0 +1,114 @@
+const socket = io();
+
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $buttonSendLocation = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
+// Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const autoScroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
+
+  // Height of the last new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // Visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight; // If we always scroll down, just use this line.
+  }
+};
+
+socket.on("message", (message) => {
+  const html = Mustache.render(messageTemplate, {
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:m a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoScroll();
+});
+
+socket.on("locationMessage", (location) => {
+  const html = Mustache.render(locationTemplate, {
+    username: location.username,
+    url: location.url,
+    createdAt: moment(location.createdAt).format("h:m a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoScroll();
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+  document.querySelector("#sidebar").innerHTML = html;
+});
+
+$messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+  const message = event.target.elements.message.value;
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message delivered!");
+  });
+});
+
+$buttonSendLocation.addEventListener("click", (event) => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not sopported by your browser");
+  }
+
+  $buttonSendLocation.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        $buttonSendLocation.removeAttribute("disabled");
+        console.log("Location shared!");
+      }
+    );
+  });
+});
+
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
+});
---
+++ src/utils/users.js
@@ -0,0 +1,61 @@
+const users = [];
+
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase().trim() + string.slice(1);
+}
+
+const addUser = ({ id, username, room }) => {
+  // Clean the data
+  username = capitalizeFirstLetter(username);
+  room = capitalizeFirstLetter(room)
+
+  // Validate the data
+  if (!username || !room) {
+    return {
+      error: "Username and room are required",
+    };
+  }
+
+  // Check for existing users
+  const existingUsers = users.find(
+    (user) => user.room === room && user.username === username
+  );
+
+  // Validate username
+  if (existingUsers) {
+    return {
+      error: "Username is currently online",
+    };
+  }
+
+  // Store User
+  const user = { id, username, room };
+  users.push(user);
+
+  return {
+    user,
+  };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id); // Filter doesn't work because it continues searching but with findIndex stops.
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
